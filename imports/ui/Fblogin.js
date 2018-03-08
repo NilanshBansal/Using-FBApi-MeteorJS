5a55f77c7d4f2af9f0c6a7c9d5051f73500994e2
Nilansh Bansal <bansalnilansh@gmail.com>
2018-03-08 21:03:14 +0530

    bind method to pass this

--- imports/ui/Fblogin.js
+++ imports/ui/Fblogin.js
@@ -7,23 +7,27 @@ class Fblogin extends Component{
         this.state={auth:false,token:""};
     }
     login(){
-        // this.setState({auth:,token:});
-        let that=this;
+        // let that=this;
         
         FB.login(function(response) {
         
             console.log(response);
             if (response.authResponse) {
-                that.setState({auth:true,token:response.authResponse.accessToken});
+                this.setState({auth:true,token:response.authResponse.accessToken});
              console.log('Welcome!  Fetching your information.... ');
             
             } else {
              console.log('User cancelled login or did not fully authorize.');
             }
+        }.bind(this));
+    }
+    logout(){
+        FB.logout(function(response){
+            console.log("Successfully logged out!");
         });
     }
     render(){
-        return (<button className="fb-login-button btn btn-lg" onClick={this.login.bind(this)}>FB Login</button>)
+        return (<div><button className="btn btn-lg btn-primary" onClick={this.login.bind(this)}>FB Login</button><button className="btn btn-lg btn-danger" onClick={this.logout}>FB Logout</button></div>)
     }
 }
 
